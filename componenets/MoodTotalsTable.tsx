9e7b4e4b4ea5ca29357c0f9854f650b26f5dfde3
Louis Devlin <72494457+Louis-Devlin@users.noreply.github.com>
2024-01-23 20:42:44 +0000

    WU-36: Display Emoji for mood group

--- componenets/MoodTotalsTable.tsx
+++ componenets/MoodTotalsTable.tsx
@@ -5,19 +5,35 @@ type ItemProps = {
 };
 
 export default function MoodTotalsTable({ moodData }: ItemProps) {
+    const getEmojiByColor = (color: string) => {
+        switch(color){
+            case "red":
+                return "😠"
+            case "blue":
+                return "😞 😴"
+            case "green":
+                return "😄 😴"
+            case "yellow":
+                return "😁"
+            default:
+                return "😞 😴"
+        }
+
+    }
   return (
     <DataTable>
       <DataTable.Header>
         <DataTable.Title>Mood</DataTable.Title>
         <DataTable.Title>Count</DataTable.Title>
-        <DataTable.Title>Colour</DataTable.Title>
+        <DataTable.Title>Mood Group</DataTable.Title>
       </DataTable.Header>
       {moodData?.map((item: any, index: number) => {
+        const emoji = getEmojiByColor(item.colour)
         return (
           <DataTable.Row key={index}>
             <DataTable.Cell>{item.moodName}</DataTable.Cell>
             <DataTable.Cell>{item.count}</DataTable.Cell>
-            <DataTable.Cell>{item.colour}</DataTable.Cell>
+            <DataTable.Cell>{emoji}</DataTable.Cell>
           </DataTable.Row>
         );
       })}
