6b88e299170bc9a7af18dd6ddaf25834170dfc29
Louis Devlin <72494457+Louis-Devlin@users.noreply.github.com>
2024-01-23 12:31:18 +0000

    WU-36: Modify calendarto also display mood log

--- screens/PastDays.tsx
+++ screens/PastDays.tsx
@@ -5,16 +5,23 @@ import { RootStackParamList } from "../Types/RootStackParamList";
 import { ApiCall } from "../functions/ApiCall";
 import { HabitTotals } from "../Types/HabitTotals";
 import { DataTable } from "react-native-paper";
+import { MoodTotals } from "../Types/MoodTotals";
 
 type Props = NativeStackScreenProps<RootStackParamList, "PastLogs">;
 
 export default function PastDays({ route, navigation }: Props) {
-  const [log, setLog] = useState<HabitTotals[]>([]);
+  const [habitLog, setHabitLog] = useState<HabitTotals[]>([]);
+  const [moodLog, setMoodLog] = useState<MoodTotals>();
   const { date } = route.params;
   useEffect(() => {
     ApiCall.getLoggedHabitsByDate(0, date, false).then((res) => {
-      setLog(res.responseData.data.filter((x: HabitTotals) => x.count > 0));
+      setHabitLog(
+        res.responseData.data.filter((x: HabitTotals) => x.count > 0)
+      );
       console.log(res.responseData.data);
+      ApiCall.GetLoggedMoodsByDate(0, date).then((res) => {
+        setMoodLog(res);
+      });
     });
   }, []);
   return (
@@ -24,12 +31,36 @@ export default function PastDays({ route, navigation }: Props) {
           <DataTable.Title>Habit</DataTable.Title>
           <DataTable.Title>Count</DataTable.Title>
         </DataTable.Header>
-        {log?.slice(0, log.length).map((item: HabitTotals, index: number) => (
-          <DataTable.Row key={index}>
-            <DataTable.Cell>{item.habitName}</DataTable.Cell>
-            <DataTable.Cell>{item.count}</DataTable.Cell>
-          </DataTable.Row>
-        ))}
+        {habitLog
+          ?.slice(0, habitLog.length)
+          .map((item: HabitTotals, index: number) => (
+            <DataTable.Row key={index}>
+              <DataTable.Cell>{item.habitName}</DataTable.Cell>
+              <DataTable.Cell>{item.count}</DataTable.Cell>
+            </DataTable.Row>
+          ))}
+      </DataTable>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>Mood</DataTable.Title>
+          <DataTable.Title>Count</DataTable.Title>
+        </DataTable.Header>
+        <DataTable.Row>
+          <DataTable.Cell>😠</DataTable.Cell>
+          <DataTable.Cell>{moodLog?.red}</DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row>
+          <DataTable.Cell>😞 😴</DataTable.Cell>
+          <DataTable.Cell>{moodLog?.blue}</DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row>
+          <DataTable.Cell>😄 😴</DataTable.Cell>
+          <DataTable.Cell>{moodLog?.green}</DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row>
+          <DataTable.Cell>😁</DataTable.Cell>
+          <DataTable.Cell>{moodLog?.yellow}</DataTable.Cell>
+        </DataTable.Row>
       </DataTable>
     </View>
   );
